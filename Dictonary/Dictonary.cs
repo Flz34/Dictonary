983f33f4fc3e8cc57ea789a52a11a8c4c3f33349
FİLİZ KAHRIMAN <FILIZKAHRIMAN@DESKTOP-RAMVN4B>
2021-02-16 12:57:15 +0300

    4. Gün Ödev

---
+++ Dictonary/Dictonary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictonary
+{
+    class Dictonary<T,Y>
+    {
+        T[] keys;
+        Y[] values;
+        T[] tempKeys;
+        Y[] tempValues;
+
+        public Dictonary()
+        {
+            keys = new T[0];
+            values = new Y[0];
+        }
+
+        public void Add(T key, Y value)
+        {
+            tempKeys = keys;
+            tempValues = values;
+
+            keys = new T[keys.Length + 1];
+            values = new Y[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+       
+
+        public int Count
+        {
+            get { return values.Length; }
+        }
+
+
+    }
+}
---
+++ Dictonary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictonary<int, string> plakalarveSehirler = new Dictonary<int, string>();
+
+            plakalarveSehirler.Add(25, "Erzurum");
+            plakalarveSehirler.Add(34, "İstanbul");
+            plakalarveSehirler.Add(06, "Ankara");
+            plakalarveSehirler.Add(02, "Adıyaman");
+            plakalarveSehirler.Add(01, "Adana");
+            plakalarveSehirler.Add(23, "Elazığ");
+           
+            Console.WriteLine(plakalarveSehirler.Count);
+        }
+    }
+}
